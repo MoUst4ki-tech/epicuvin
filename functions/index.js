fc84d895c04e6b69ff686770b553043945085d0b
MoUst4ki <noe1.marchal@epitech.eu>
2026-02-13 22:15:29 +0100

    change model llm

--- functions/index.js
+++ functions/index.js
@@ -38,7 +38,7 @@ Fais des réponses concises, naturelles, sans utiliser trop de formatage (évite
 `;
 
         // Appel à l'IA
-        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+        const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
         const result = await model.generateContent(prompt);
         const responseText = result.response.text();
 
